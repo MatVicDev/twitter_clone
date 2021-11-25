3da8a5e76f84f8261844c60df224f899830ec50f
MatVicDev <matheus@teste.com>
2021-11-25 09:44:12 -0300

    Adicionando a timeline e listando os tweets

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -9,14 +9,37 @@ class AppController extends Action
 {
 	public function timeline()
 	{
+		$this->validarAutenticacao();
+
+		$tweet = Container::getModel('Tweet');
+
+		$tweet->__set('fk_id_usuario', $_SESSION['id_usuario']);
+
+		$this->view->tweets = $tweet->getAll();
+
+		$this->render('timeline');
+	}
+
+	public function tweet()
+	{
+		$this->validarAutenticacao();
+			
+		$tweet = Container::getModel('Tweet');
+		$tweet->__set('tweets', $_POST['tweet']);
+		$tweet->__set('fk_id_usuario', $_SESSION['id_usuario']);
+
+		$tweet->salvar();
+
+		header('Location: /timeline');
+	}
+
+	public function validarAutenticacao()
+	{
 		session_start();
 
-		// Se o usuário foi autenticado
-		if(!empty($_SESSION['id_usuario']) && !empty($_SESSION['nome'])) {
-			$this->render('timeline');
-		} else {
+		if(!isset($_SESSION['id_usuario']) || $_SESSION['id_usuario'] == '' || 
+			!isset($_SESSION['nome']) || $_SESSION['nome'] == '')
 			header('Location: /?login=erro');
-		}
 	}
 }
 ?>
---
+++ App/Models/Tweet.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use MF\Model\Model;
+
+class Tweet extends Model
+{
+	private $id_tweet;
+	private $fk_id_usuario;
+	private $tweets;
+	private $data;
+
+	public function __get($attr)
+	{
+		return $this->$attr;
+	}
+
+	public function __set($attr, $value)
+	{
+		$this->$attr = $value;
+	}
+
+	// Salvar
+	public function salvar()
+	{
+		$query = 'INSERT INTO tb_tweets(fk_id_usuario, tweets)VALUES(:FK_ID_USUARIO, :TWEETS)';
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':FK_ID_USUARIO', $this->__get('fk_id_usuario'));
+		$stmt->bindValue(':TWEETS', $this->__get('tweets'));
+		$stmt->execute();
+
+		return $this;
+	}
+
+	// Recuperar
+	public function getAll()
+	{
+		$query = '
+			SELECT 
+				t.id_tweet, t.fk_id_usuario, u.nome, t.tweets, 
+				DATE_FORMAT(t.data, "%d/%m/%Y %H:%i") as data
+			FROM 
+				tb_tweets as t
+			LEFT JOIN 
+				tb_usuarios as u 
+			ON
+				(t.fk_id_usuario = u.id_usuario)
+			WHERE 
+				t.fk_id_usuario = :FK_ID_USUARIO
+			ORDER BY
+				t.data DESC';
+
+		$stmt = $this->db->prepare($query);
+		$stmt->bindValue(':FK_ID_USUARIO', $this->__get('fk_id_usuario'));
+		$stmt->execute();
+
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
+}
+?>
